4b9db28f8a95f16e79319764c2f72d2d533220ec
蓝华建 <huajianlan@rastar.com>
2019-08-24 18:52:09 +0800

    1

--- app/src/main/java/com/example/lanhuajian/blues/constant/RequestUrl.java
+++ app/src/main/java/com/example/lanhuajian/blues/constant/RequestUrl.java
@@ -4,11 +4,15 @@ public class RequestUrl {
 
     public static final String BASE_GANKIO_URL = "http://gank.io";
 
+    //本地服务器
     public static final String BASE_LOCAL_URL = "http://192.168.33.93:8090/";
 
     public static final String BASE_MOCK_URL = "https://easy-mock.com/";
 
     public static final String BASE_WANANDROID_URL = "https://www.wanandroid.com/";
+
+    //微博内容url
+    public static final String BASE_WEIBO_URL = "https://m.weibo.cn/api/container/getIndex?containerid=102803";
 
 
 }
